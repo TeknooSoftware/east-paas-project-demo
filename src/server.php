d14c80707ad061d323d452c69bdc7563f8be0f8f
Richard Déloge <richarddeloge@gmail.com>
2022-10-17 20:48:53 +0200

    Update returned value to test feature

--- src/server.php
+++ src/server.php
@@ -11,19 +11,19 @@ require(__DIR__ . '/../vendor/autoload.php');
 $loop = Loop::get();
 
 $server = new HttpServer($loop, static function () {
+    $extra = 'Missing extra';
+    if (\file_exists('/opt/extra/extra/run.php')) {
+        $extra = 'extra : ' . include('/opt/extra/extra/run.php');
+    }
+
     $output = 'Hello World! '
         . PHP_EOL . 'Date : ' . \date('Y-m-d H:i:s')
         . PHP_EOL . 'volume-vault.foo : ' . @\file_get_contents('/vault/foo')
         . PHP_EOL . 'volume-vault.bar : ' . @\file_get_contents('/vault/bar')
         . PHP_EOL . 'map-vault.key1 : ' . ($_ENV['KEY1'] ?? '')
         . PHP_EOL . 'map-vault.key2 : ' . ($_ENV['KEY2'] ?? '')
-        . PHP_EOL . 'output' . @\file_get_contents('/opt/data/output');
-
-    if (\file_exists('/opt/extra/extra/run.php')) {
-        $output .= PHP_EOL . 'extra : ' . include('/opt/extra/extra/run.php');
-    } else {
-        $output .= PHP_EOL . 'Missing extra : ';
-    }
+        . PHP_EOL . 'output : ' . @\file_get_contents('/opt/data/output')
+        . PHP_EOL . $extra;
 
     @\file_put_contents('/opt/data/output', time());
 
