a28f5ee54d18bf85f61bb1fd4fddab84bbc91b5b
Richard Deloge <richarddeloge@gmail.com>
2023-06-29 09:15:13 +0200

    add python demo

---
+++ python-app/server.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+PORT = 8765
+MESSAGE = "Hello World!"
+
+app = Flask(__name__)
+
+@app.route("/")
+def root():
+    result = MESSAGE.encode(encoding="utf-8")
+    return result
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=PORT)
