a443f84a3ba197def59ecfb69a373a6777b47e52
Richard Deloge <richarddeloge@gmail.com>
2025-11-11 22:48:27 +0100

    Update libs

--- python-app/server.py
+++ python-app/server.py
@@ -12,4 +12,4 @@ def catch_all(path):
     return result
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=PORT)
+    app.run(debug=True, host="0.0.0.0", port=PORT, use_reloader=False)
